c76317ca1c899cdee85738e4d269b44ac454e290
shamlikhan <shamlikhan@qburst.com>
2019-11-15 21:52:29 +0530

    CORS added

--- api/serve.go
+++ api/serve.go
@@ -3,11 +3,16 @@ package api
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/handlers"
 )
 
 // Serve : Run api server
 func (a *App) Serve(addr string) {
 	router := a.Router()
+	allowedHeaders := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"})
 	log.Println("App : Server is listening...")
-	http.ListenAndServe(addr, router)
+	http.ListenAndServe(addr, handlers.CORS(allowedHeaders, allowedOrigins, allowedMethods)(router))
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module qk-note
 go 1.13
 
 require (
+	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
 	github.com/lib/pq v1.2.0
 )
